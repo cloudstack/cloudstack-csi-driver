a84477e922d62b82387ab55134fafc9c0b5aaf64
Elias Flötzinger <eliasfloetzinger@gmx.de>
2026-09-01 22:12:28 +0200

    Fix bug in cloud client missing the project id and refactor the package (#7)
    * fix: use project id when getting VM by name
    
    * refactor: remove global projectID from cloudstack client
    
    * refactor: require arguments in snapshot functions
    
    * refactor: use project id default in cloudstack methods
    
    * docs: update default project id option description
    
    * fix: restore count checks in vms, volumes, snapshots
    
    * fix: add ttl for syncer job to allow helm upgrade
    
    * refactor: remove dead code in cloud package

--- pkg/cloud/vms.go
+++ pkg/cloud/vms.go
@@ -27,27 +27,18 @@ import (
 
 func (c *client) GetVMByID(ctx context.Context, vmID string) (*VM, error) {
 	logger := klog.FromContext(ctx)
-	p := c.VirtualMachine.NewListVirtualMachinesParams()
-	p.SetId(vmID)
-	if c.projectID != "" {
-		p.SetProjectid(c.projectID)
-	}
-	logger.V(2).Info("CloudStack API call", "command", "ListVirtualMachines", "params", map[string]string{
-		"id":        vmID,
-		"projectID": c.projectID,
+	logger.V(2).Info("CloudStack API call", "command", "GetVirtualMachineByID", "params", map[string]string{
+		"id": vmID,
 	})
-	l, err := c.VirtualMachine.ListVirtualMachines(p)
+
+	vm, count, err := c.VirtualMachine.GetVirtualMachineByID(vmID)
 	if err != nil {
+		if count == 0 {
+			return nil, ErrNotFound
+		}
+
 		return nil, err
 	}
-	if l.Count == 0 {
-		return nil, ErrNotFound
-	}
-	if l.Count > 1 {
-		return nil, ErrTooManyResults
-	}
-	vm := l.VirtualMachines[0]
-	logger.V(2).Info("Returning VM", "vmID", vm.Id, "zoneID", vm.Zoneid)
 
 	return &VM{
 		ID:     vm.Id,
@@ -57,22 +48,18 @@ func (c *client) GetVMByID(ctx context.Context, vmID string) (*VM, error) {
 
 func (c *client) getVMByName(ctx context.Context, name string) (*VM, error) {
 	logger := klog.FromContext(ctx)
-	p := c.VirtualMachine.NewListVirtualMachinesParams()
-	p.SetName(name)
-	logger.V(2).Info("CloudStack API call", "command", "ListVirtualMachines", "params", map[string]string{
+	logger.V(2).Info("CloudStack API call", "command", "GetVirtualMachineByName", "params", map[string]string{
 		"name": name,
 	})
-	l, err := c.VirtualMachine.ListVirtualMachines(p)
+
+	vm, count, err := c.VirtualMachine.GetVirtualMachineByName(name)
 	if err != nil {
+		if count == 0 {
+			return nil, ErrNotFound
+		}
+
 		return nil, err
 	}
-	if l.Count == 0 {
-		return nil, ErrNotFound
-	}
-	if l.Count > 1 {
-		return nil, ErrTooManyResults
-	}
-	vm := l.VirtualMachines[0]
 
 	return &VM{
 		ID:     vm.Id,
